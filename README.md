85fe09fc0b4afa64149a0935f891ab95e011590e
Taylor Smock <taylor.smock@kaartgroup.com>
2019-08-29 08:38:40 -0600

    Skeleton from JOSM's 00_plugin_dir_template, minus empty directories, with modifications for actual compilation
    Signed-off-by: Taylor Smock <taylor.smock@kaartgroup.com>

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # lane-connectivity-plugin
 JOSM GUI plugin to facilitate quick and accurate tagging for lane connectivity relations
+
+# Authors
+* Andrew Piechota
+* Farris Billy
+* Taylor Smock <taylor.smock@kaartgroup.com>
---
+++ src/com/kaart/laneconnectivity/LaneConnectivity.java
@@ -0,0 +1,46 @@
+package com.kaart.laneconnectivity;
+
+import static org.openstreetmap.josm.tools.I18n.tr;
+
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+
+import javax.swing.AbstractAction;
+
+import org.openstreetmap.josm.gui.MainApplication;
+import org.openstreetmap.josm.gui.preferences.PreferenceSetting;
+import org.openstreetmap.josm.plugins.Plugin;
+import org.openstreetmap.josm.plugins.PluginInformation;
+import org.openstreetmap.josm.spi.preferences.Config;
+import org.openstreetmap.josm.tools.ImageProvider;
+import org.openstreetmap.josm.tools.Logging;
+
+/**
+ *
+ * @author Taylor Smock
+ *
+ */
+public class LaneConnectivity extends Plugin {
+	public static final String NAME = "Lane Connectivity";
+
+	public static final String PLUGIN_IMAGE = null; // TODO get an image
+
+	public LaneConnectivity(PluginInformation info) {
+		super(info);
+		AbstractAction laneConnectivityAction = new AbstractAction(NAME.concat(tr(" window")),
+				ImageProvider.get(PLUGIN_IMAGE, ImageProvider.ImageSizes.MENU)) {
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO make a window
+			}
+		};
+		MainApplication.getMenu().dataMenu.add(laneConnectivityAction);
+	}
+
+	public static String getVersion() {
+		// TODO get the version dynamically
+		return "v0.0.1";
+	}
+}
