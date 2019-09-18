755ea2b97c1e6b10460bce0d1d42d56970cf212b
Taylor Smock <taylor.smock@kaart.com>
2019-09-18 14:50:13 -0600

    Fix sonarlint issues with Constants class
    Signed-off-by: Taylor Smock <taylor.smock@kaart.com>

--- src/com/kaart/laneconnectivity/model/Constants.java
+++ src/com/kaart/laneconnectivity/model/Constants.java
@@ -3,27 +3,30 @@ package com.kaart.laneconnectivity.model;
 
 import java.util.regex.Pattern;
 
-public interface Constants {
-    String SEPARATOR = ";";
-    String SPLIT_REGEX = "\\p{Zs}*[,:;]\\p{Zs}*";
-    Pattern SPLIT_PATTERN = Pattern.compile(SPLIT_REGEX);
+public final class Constants {
+	private Constants() {
+		// Do nothing
+	}
+    static final String SEPARATOR = ";";
+    static final String SPLIT_REGEX = "\\p{Zs}*[,:;]\\p{Zs}*";
+    static final Pattern SPLIT_PATTERN = Pattern.compile(SPLIT_REGEX);
 
-    String TYPE_LENGTHS = "turnlanes:lengths";
+    static final String TYPE_LENGTHS = "turnlanes:lengths";
 
-    String LENGTHS_KEY_LENGTHS_LEFT = "lengths:left";
-    String LENGTHS_KEY_LENGTHS_RIGHT = "lengths:right";
+    static final String LENGTHS_KEY_LENGTHS_LEFT = "lengths:left";
+    static final String LENGTHS_KEY_LENGTHS_RIGHT = "lengths:right";
 
-    String TYPE_TURNS = "turnlanes:turns";
-    String TYPE_CONNECTION = "connectivity";
-    String TYPE_CONNECTIVITY = "1:1|2:2|3:3|4:4";
+    static final String TYPE_TURNS = "connectivity";
+    static final String TYPE_CONNECTION = "connectivity";
+    static final String TYPE_CONNECTIVITY = "1:1|2:2|3:3|4:4";
 
-    String TURN_ROLE_VIA = "via";
-    String TURN_ROLE_FROM = "from";
-    String TURN_ROLE_TO = "to";
+    static final String TURN_ROLE_VIA = "via";
+    static final String TURN_ROLE_FROM = "from";
+    static final String TURN_ROLE_TO = "to";
 
-    String TURN_KEY_LANES = "lanes";
-    String TURN_KEY_EXTRA_LANES = "lanes:extra";
-    String LENGTHS_ROLE_END = "end";
-    String LENGTHS_ROLE_WAYS = "ways";
+    static final String TURN_KEY_LANES = "lanes";
+    static final String TURN_KEY_EXTRA_LANES = "lanes:extra";
+    static final String LENGTHS_ROLE_END = "end";
+    static final String LENGTHS_ROLE_WAYS = "ways";
 
 }
